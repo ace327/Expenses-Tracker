abe84c09ebe49ace55c7a58045c31173cc34ecf3
ace327 <aryasuthar2002@gmail.com>
2026-01-20 02:54:03 +0530

    hii

--- frontend/expenses-tracker/src/App.jsx
+++ frontend/expenses-tracker/src/App.jsx
@@ -1,11 +1,53 @@
 import React from 'react'
 
+import { BrowserRouter as  Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+import Login from './pages/auth/Login';
+import SignUp from './pages/auth/SignUp';
+import Home from './pages/Dashboard/Home';
+import Income from './pages/Dashboard/Income';
+import Expense from './pages/Dashboard/Expense';
+
+
 const App = () => {
   return (
-    <div className='text-5xl'>
-      App 
+    
+    <div>
+      <Router>
+        <Routes>
+
+          <Route path="/" element={<Root />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/signUp" exact element={<SignUp />} />
+          <Route path="/dashboard" exact element={<Home />} />
+          <Route path="/income" exact element={<Income />} />
+          <Route path="/expense" exact element={<Expense />} />
+
+
+
+        </Routes>
+      </Router>
     </div>
   )
 }
-
+ 
 export default App
+
+
+const Root = () => {
+
+  //check if token exists in local storage
+  const isAuntheticated = !!localStorage.getItem("token");
+
+  //Redirect to dashboard if auntheticated, otherwise to login
+  return isAuntheticated ? (
+    <Navigate to="/dashboard" /> 
+  ) : (
+
+    <Navigate to="/login" />
+  )
+}
---
+++ frontend/expenses-tracker/src/utils/uploadImage.js
@@ -0,0 +1,1 @@
+import { API_PATHS} from './apiPaths';
